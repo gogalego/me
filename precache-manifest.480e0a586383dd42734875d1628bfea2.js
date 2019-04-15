f6ebcc94bed781228f8dbfff16db993435860d9b
GoGalego <galego2320@hotmail.com>
2019-04-15 18:22:56 +0100

    Updates

---
+++ precache-manifest.480e0a586383dd42734875d1628bfea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d3a57f721d0ca4183823dd23d5207ab",
+    "url": "/gogalego/me/static/media/header.5d3a57f7.jpg"
+  },
+  {
+    "revision": "676ffb1431ed1c9d16c4",
+    "url": "/gogalego/me/static/js/runtime~main.d0a407cc.js"
+  },
+  {
+    "revision": "1c755b8d0653042b80c5",
+    "url": "/gogalego/me/static/js/main.4ca52d7c.chunk.js"
+  },
+  {
+    "revision": "06e780bfe6a4bdf64dab",
+    "url": "/gogalego/me/static/js/2.3773d23c.chunk.js"
+  },
+  {
+    "revision": "1c755b8d0653042b80c5",
+    "url": "/gogalego/me/static/css/main.5204c11b.chunk.css"
+  },
+  {
+    "revision": "bbb9f00d94d9333d16d3169aa97be2cb",
+    "url": "/gogalego/me/index.html"
+  }
+];

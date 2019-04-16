be1f30c02abee6f9682fe10d824f290e324dc2ab
GoGalego <galego2320@hotmail.com>
2019-04-16 13:51:00 +0100

    Updates

---
+++ precache-manifest.f72f53cba352cd5b28d1dcb6581c20b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d3a57f721d0ca4183823dd23d5207ab",
+    "url": "/me/static/media/header.5d3a57f7.jpg"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "6cc067c4fc86462b7002",
+    "url": "/me/static/js/main.9c0fe783.chunk.js"
+  },
+  {
+    "revision": "06e780bfe6a4bdf64dab",
+    "url": "/me/static/js/2.3773d23c.chunk.js"
+  },
+  {
+    "revision": "6cc067c4fc86462b7002",
+    "url": "/me/static/css/main.12cb68fb.chunk.css"
+  },
+  {
+    "revision": "d4405085f846dafaab288189a0b0ec60",
+    "url": "/me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gogalego/me/precache-manifest.480e0a586383dd42734875d1628bfea2.js"
+  "/me/precache-manifest.f72f53cba352cd5b28d1dcb6581c20b7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/gogalego/me/index.html", {
+workbox.routing.registerNavigationRoute("/me/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
